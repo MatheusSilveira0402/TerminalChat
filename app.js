abfdff2bebf1edf78c3661cf041d1d079257ee80
MatheusSilveiraSergio0402 <matheussilveirasergio@gmail.com>
2021-08-30 19:22:05 -0300

    subindo a navbar

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 /* importar as configurações do servidor */
 var app = require('./config/server');
 
+var port = 80
 /* parametrizar a porta de escuta */
-app.listen(8001, function(){
-	console.log('Servidor online');
+app.listen(port, function(){
+	console.log('Servidor online na porta:',port);
 })
